67836835d98794fef12226a5e878c6df04c826f6
Brian.Jordan <brianjordan283@gmail.com>
2026-03-18 15:52:15 +0100

    create posts slice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState: {
+    posts: [],
+    isLoading: false,
+    error: null
+  },
+  reducers: {}
+})
+
+export default postsSlice.reducer
